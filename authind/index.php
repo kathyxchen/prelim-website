57b489f3d18397d2e35e8bed3dd533eb5cea9480
Kathy <kchen2013@gmail.com>
2014-07-28 13:11:12 +0000

    Fixed author's index formatting

--- authind/index.php
+++ authind/index.php
@@ -4,7 +4,6 @@
   <title>groundwork.</title>
 </head>
 <body>
-  <div id="container">
     <div id="menu"> 
         <?php
             $menu = array(
@@ -22,23 +21,37 @@
     </div>
     <div id="buffer">
     </div>
-    <div id="index">
+    <div id="container">
       <h2><br />The Authors</h2>
+      <div id="index">
       <?php
         include("../dbinfo.inc.php");
         $mysqli = new mysqli($host, $username, $password, $database);
         if ($mysqli->connect_errno) {
           echo "Failed";
         }
-        
+       
+       function check($email) {
+          if (!$email || $email == '') {
+            return '';
+          }
+          else {
+            return '<span class="mail"> (' . '<a class="mlink" href="mailto:' . $email . '">contact' . '</a>) </span>';
+          }
+       }
+
+        function convertTime($d) {
+          $ts = strtotime($d);
+          return date("F d, Y", $ts);
+        }
         function match_title($ia, $mysqli) {
-          $query = "SELECT title, ID FROM articles WHERE author_id=" . $ia . " ORDER BY post_time DESC";
+          $query = "SELECT title, post_time, ID FROM articles WHERE author_id=" . $ia . " ORDER BY post_time DESC";
           if ($stmt = $mysqli->prepare($query)) {
             $stmt->execute();
             $stmt->store_result();
-            $stmt->bind_result($t, $it);
+            $stmt->bind_result($t, $pt, $it);
             while ($stmt->fetch()) {
-              echo '<ul class="title"><a href="../article.php?id=' . $it . '">' . $t . '</a></ul>';
+              echo '<ul class="title"><a href="../article.php?id=' . $it . '">' . $t . ' (' . convertTime($pt) . ')</a></ul>';
             }
           }
         }
@@ -50,12 +63,13 @@
             $stmt->bind_result($n, $e, $ia);
             while ($stmt->fetch()) {
               echo '<div class="auth">' . $n;
+              echo check($e);
               match_title($ia, $mysqli);
               echo '</div>';
             }
         }
       ?>
+      </div>
     </div>
-  </div>
 </body>
 </html>
